dec5362bf765944fe0c5947d923474ed6e6023dc
vlaurac <128168410+vlaurac@users.noreply.github.com>
2023-07-23 14:35:24 -0500

    vistas
    el mariposario es para que distingan el tipo de mariposas y clases en el cefa

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mariposario cefa</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+
+    <header class="bg_animate">
+        <div class="header_nav">
+            <div class="contenedor">
+                <h1>mariposario</h1>
+                <nav>
+                    <a href="#">inicio</a>
+                    <a href="#">nosotros</a>
+                    <a href="#">contacto</a>
+                </nav>
+            </div>
+        </div> 
+
+        <section class="banner contenedor">
+            <secrion class="banner_title">
+                <h2>conocimiento de las mariposas <br>en el cefa</h2>
+                <a href="#" class="conocenos btn">conocenos</a>
+            </secrion>
+            <div class="banner_img">
+                <img src="mariposa sena.png" alt="">
+            </div>
+        </section>
+        <div class="burbujas">
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+        </div>
+    </header>
+    <div class="container">
+        <div class="card">
+            <figure>
+                <img src="" alt="">
+            </figure>
+            <div class="contenido">
+                <h3>profesor</h3>
+                <p></p>
+                <a href="#">mas...</a>
+            </div>
+        </div>
+        <div class="card">
+            <figure>
+                <img src="" alt="">
+            </figure>
+            <div class="contenido">
+                <h3>profesor</h3>
+                <p></p>
+                <a href="#">mas...</a>
+            </div>
+        </div>
+        <div class="card">
+            <figure>
+                <img src="" alt="">
+            </figure>
+            <div class="contenido">
+                <h3>profesor</h3>
+                <p></p>
+                <a href="#">mas...</a>
+            </div>
+        </div>
+        <div class="card">
+            <figure>
+                <img src="" alt="">
+            </figure>
+            <div class="contenido">
+                <h3>profesor</h3>
+                <p></p>
+                <a href="#">mas...</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
